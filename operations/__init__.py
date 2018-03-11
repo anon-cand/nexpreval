fdee822773645d75ddf5357055b6761b5429f311
abhyaasi <34417624+abhyaasi@users.noreply.github.com>
2018-03-11 22:24:17 +0800

    Hidden Operations imports from clients

--- operations/__init__.py
+++ operations/__init__.py
@@ -1,24 +1,29 @@
 import logging
 from logging import NullHandler
+from .operation import Operation as _Operation
+from .constant import Constant as _Constant
+from .addition import Addition as _Addition
+from .subtraction import Subtraction as _Subtraction
+from .multiplication import Multiplication as _Multiplication
+from .division import Division as _Division
+
+__all__ = ['catalogue']
 
 logging.getLogger(__name__).addHandler(NullHandler())
 
-from .operation import Operation
-from .constant import Constant
-from .addition import Addition
-from .subtraction import Subtraction
-from .multiplication import Multiplication
-from .division import Division
 
 def catalogue():
     """
     Loads operations and constructs a mapping out of them
     :return: Mapping of operation tag and Operation class
     """
-    catalog = {sc.TAG: sc for sc in Operation.__subclasses__()}
+    catalog = {sc.TAG: sc for sc in _Operation.__subclasses__()}
     return catalog
 
-__all__ = ['catalogue']
+
+
+
+
 
 
 
